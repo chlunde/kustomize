d2069cbd4e506d3f97e9ccfd8411d7f340531c68
Carl Henrik Lunde <chlunde@ifi.uio.no>
2023-06-26 12:00:30 +0200

    Profile

--- kustomize/main.go
+++ kustomize/main.go
@@ -5,14 +5,34 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"os"
+	"runtime/pprof"
 
 	"sigs.k8s.io/kustomize/kustomize/v5/commands"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+	}
+
 	if err := commands.NewDefaultCommand().Execute(); err != nil {
+		if *cpuprofile != "" {
+			pprof.StopCPUProfile()
+		}
 		os.Exit(1)
+	}
+	if *cpuprofile != "" {
+		pprof.StopCPUProfile()
 	}
 	os.Exit(0)
 }
